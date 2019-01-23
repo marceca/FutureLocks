d5998de0f10891bb9d054bc78690ff86ca2f94ad
Mike <mmarceca@aardvarkbrigade.com>
2019-01-23 16:34:47 -0500

    started styling on my account pages

--- public/wp-content/themes/blankslate/gulpfile.js
+++ public/wp-content/themes/blankslate/gulpfile.js
@@ -3,6 +3,7 @@ const concat = require('gulp-concat');
 const minifyCSS = require('gulp-minify-css');
 const autoprefixer = require('gulp-autoprefixer');
 const livereload = require('gulp-livereload');
+const sass = require('gulp-sass');
 
 gulp.task('css', function() {
   return gulp.src('assets/css/**/*.css')
@@ -10,10 +11,17 @@ gulp.task('css', function() {
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
     .pipe(concat('style.css'))
     .pipe(gulp.dest('.'))
-    .pipe(livereload());
+    .pipe(livereload())
 });
 
+gulp.task('sass', function() {
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'))
+})
+
 gulp.task('watch', function() {
+  gulp.watch('assets/sass/**/*.scss', gulp.series('sass'));
   gulp.watch('assets/css/**/*.css', gulp.series('css'));
   livereload.listen();
 });
--- public/wp-content/themes/blankslate/page.php
+++ public/wp-content/themes/blankslate/page.php
@@ -1,3 +1,8 @@
+<?php
+	$url = explode('/',$_SERVER['REQUEST_URI']);
+	$dir = $url[1] ? $url[1] : 'home';
+?>
+
 <?php get_header(); ?>
 <section id="content" role="main" class="body-wrapper">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
@@ -5,13 +10,12 @@
 <header class="header">
 <?php edit_post_link(); ?>
 </header>
-<section class="entry-content">
+<section class="entry-content" id="<?php echo $dir ?>">
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
 </article>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
 <?php endwhile; endif; ?>
 </section>
 <?php get_footer(); ?>
